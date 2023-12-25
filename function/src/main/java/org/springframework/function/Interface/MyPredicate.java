eb78582544d40f1e08a3698b52ee805ba69e2f3a
王宇 <18766267141@163.com>
2023-12-25 18:00:18 +0800

    新增函数操作

---
+++ function/src/main/java/org/springframework/function/Interface/MyPredicate.java
@@ -0,0 +1,7 @@
+package org.springframework.function.Interface;
+
+@FunctionalInterface
+public interface MyPredicate {
+
+    void test();
+}
--- function/src/main/java/org/springframework/function/Main.java
+++ function/src/main/java/org/springframework/function/Main.java
@@ -1,5 +1,7 @@
 package org.springframework.function;
 
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
