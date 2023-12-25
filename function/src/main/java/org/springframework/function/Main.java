8b1691e85a093fe13877cca0fcd28a3eb666f1c3
王宇 <18766267141@163.com>
2023-12-25 18:15:43 +0800

    新增函数操作

--- function/src/main/java/org/springframework/function/Main.java
+++ function/src/main/java/org/springframework/function/Main.java
@@ -1,9 +1,12 @@
 package org.springframework.function;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Consumer<String> tConsumer = System.out::print;
+        tConsumer.accept("test a main");
     }
 }
